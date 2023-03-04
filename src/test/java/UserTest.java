6880f845d99ed745ee7710eec695b5ac36c978d4
Anton Saltanov <saltanov.anton@yandex.ru>
2023-03-04 21:46:15 +0900

    commit 2

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,38 @@
-
+import org.apache.commons.lang3.StringUtils;
+import org.example.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class UserTest {
+    private static final String LOGIN_TEST = "user";
+    private static final String CORRECT_EMAIL_TEST = "user@mail.ru";
 
+
+
+    @Test
+    public void shouldBeLoginAndEmail() {
+        User user = new User(LOGIN_TEST, CORRECT_EMAIL_TEST);
+        Assertions.assertFalse(StringUtils.isEmpty(user.getLogin()));
+        Assertions.assertFalse(StringUtils.isEmpty(user.getEmail()));
+    }
+
+    @Test
+    public void shouldBeWithoutLoginAndEmail() {
+        User user = new User();
+        Assertions.assertTrue(StringUtils.isEmpty(user.getLogin()));
+        Assertions.assertTrue(StringUtils.isEmpty(user.getEmail()));
+    }
+
+    @Test
+    public void shouldBeCorrectEmail() {
+        User user = new User(LOGIN_TEST, CORRECT_EMAIL_TEST);
+        Assertions.assertTrue(StringUtils.containsAny("@", user.getEmail()));
+        Assertions.assertTrue(StringUtils.containsAny(".", user.getEmail()));
+    }
+
+    @Test
+    public void shouldBeDifferentLoginAndEmail() {
+        User user = new User(LOGIN_TEST, CORRECT_EMAIL_TEST);
+        Assertions.assertNotEquals(user.getEmail(), user.getLogin());
+    }
 }
